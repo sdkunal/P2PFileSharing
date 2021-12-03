1263038f77fe72442fe9a56493be64ce5dfe8d24
Shreeya Deshpande <41054973+ShreeyaDeshpande@users.noreply.github.com>
2021-12-02 20:16:17 -0500

    Update NeighbourPeerNode.java

--- NeighbourPeerNode.java
+++ NeighbourPeerNode.java
@@ -1,27 +1,28 @@
 
 public class NeighbourPeerNode {
 	private int peerId = -1;
-	private String hostName = "";
-	private int portNumber = -1;
+	private String host = "";
+	private int portNum = -1;
 	private int haveFile = 0;
 	private int[] bitField = null;
 
-	private NeighbourPeerNode() {}
-
-	private NeighbourPeerNode(int peerId, String hostName, int portNumber, int haveFile) {
+	private NeighbourPeerNode(int peerId, String host, int portNum, int haveFile) {
 		this.setPeerId(peerId);
-		this.setHostName(hostName);
-		this.setPortNumber(portNumber);
+		this.setHostName(host);
+		this.setPortNumber(portNum);
 		this.setHaveFile(haveFile);
 	}
+	
+	private NeighbourPeerNode() {}
+
 
 	public static NeighbourPeerNode getPeerNodeObject(String row) {		
 		String[] parameters = row.split(" ");
 		int peerId = Integer.parseInt(parameters[0]);
-		String hostName = parameters[1];
-		int portNumber = Integer.parseInt(parameters[2]);
+		String host = parameters[1];
+		int portNum = Integer.parseInt(parameters[2]);
 		int haveFile = Integer.parseInt(parameters[3]);
-		NeighbourPeerNode pn = new NeighbourPeerNode(peerId,hostName,portNumber,haveFile);
+		NeighbourPeerNode pn = new NeighbourPeerNode(peerId,host,portNum,haveFile);
 		return pn;
 	}
 
@@ -32,35 +33,35 @@ public class NeighbourPeerNode {
 	public void setPeerId(int peerId) {
 		this.peerId = peerId;
 	}
-
-	public String getHostName() {
-		return hostName;
-	}
-
-	public void setHostName(String hostName) {
-		this.hostName = hostName;
-	}
-
-	public int getPortNumber() {
-		return portNumber;
-	}
-
-	public void setPortNumber(int portNumber) {
-		this.portNumber = portNumber;
-	}
-
-	public int getHaveFile() {
-		return haveFile;
-	}
-
+	
 	public void setHaveFile(int haveFile) {
 		this.haveFile = haveFile;
 	}
-
+	
+	public int getHaveFile() {
+		return haveFile;
+	}
+	
 	public int[] getBitfield() {
 		return bitField;
 	}
+	
+	public String getHostName() {
+		return host;
+	}
 
+	public void setHostName(String host) {
+		this.host = host;
+	}
+
+	public int getPortNumber() {
+		return portNum;
+	}
+	
+	public void setPortNumber(int portNum) {
+		this.portNum = portNum;
+	}
+	
 	public void setBitfield(int[] bitfield) {
 		this.bitField = bitfield;
 	}
